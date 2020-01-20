9303de5615eb19046863ff859e461726f7e7ce10
Italo Perez <iperezmba@gmail.com>
2020-01-19 20:08:52 -0500

    Deck class

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -11,4 +11,21 @@ class Deck:
 	def __init__(self):
 		suits = ['Heart', 'Diamonds', 'Clubs', 'Spades']
 		values = ['A','2','3','4','5','6','7','8','9','10' ,'J' ,'Q','K']
-		self.cards =[ Card(va)]
+		self.cards =[ Card(value,suit) for suit in suits for value in values]
+		''' avoid to write code this way'''
+		# self.cards = []
+		# for suit in suits: 
+		# 	for value in values: 
+		# 		self.vards.append(Card(value.suit))
+		print (self.cards)
+
+
+	def __repr__(self):
+		return f"Deck of {self.count()} cards"
+
+	def count(self):
+		return len(self.cards)
+
+
+d = Deck()
+print(d)
